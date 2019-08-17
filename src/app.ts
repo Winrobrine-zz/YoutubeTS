19dbc475458d80f5e35c47e4594bd90f203a014d
박현우 <ghwhsbsb123@gmail.com>
2019-08-17 23:57:34 +0900

    Add pug partials and Init Scss

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import logger from "morgan";
+import path from "path";
 import "./utils/logger";
 
 import routes from "./routes";
@@ -12,11 +13,14 @@ import videoRouter from "./routes/video";
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger("dev"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes.index, homeRouter);
 app.use(routes.users, userRouter);
