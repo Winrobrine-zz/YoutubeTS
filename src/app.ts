0924d4c12fff879c649954b584bbc6f1cf40bb69
박현우 <ghwhsbsb123@gmail.com>
2019-08-18 00:09:18 +0900

    Local variables

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,10 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger("dev"));
+app.use((req, res, next) => {
+    res.locals.routes = routes;
+    next();
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
