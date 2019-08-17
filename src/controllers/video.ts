3e52f46ce36db1e0289cd888496779aa16cb4543
박현우 <ghwhsbsb123@gmail.com>
2019-08-18 00:19:06 +0900

    Search

--- src/controllers/video.ts
+++ src/controllers/video.ts
@@ -2,7 +2,15 @@ import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) =>
     res.render("home", { title: "Home" });
-export const search = (req: Request, res: Response) => res.send("Search");
+
+export const search = (req: Request, res: Response) => {
+    const {
+        query: { term: searchingBy }
+    } = req;
+
+    res.render("search", { title: "Search", searchingBy });
+};
+
 export const videos = (req: Request, res: Response) => res.send("Videos");
 export const upload = (req: Request, res: Response) => res.send("Upload");
 export const videoDetail = (req: Request, res: Response) =>
