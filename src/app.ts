981e03c5978894b2dbe43d6aa74450e34e9722d6
박현우 <ghwhsbsb123@gmail.com>
2019-08-17 19:33:11 +0900

    Init TypeScript & Eslint and Write test TypeScript code

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), () => {
+	console.log(
+		"  App is running at http://localhost:%d in %s mode",
+		app.get("port"),
+		app.get("env")
+	);
+	console.log("  Press CTRL-C to stop\n");
+});
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Hello, world!");
+});
