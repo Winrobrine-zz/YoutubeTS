496cc44d12767fa30718609a46d39072e4d27939
박현우 <ghwhsbsb123@gmail.com>
2019-08-17 20:30:37 +0900

    Install some middleware libraries

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import helmet from "helmet";
 import logger from "morgan";
 import "./utils/logger";
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger("dev"));
 
 app.listen(app.get("port"), () => {
