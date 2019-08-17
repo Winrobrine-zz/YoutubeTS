5227527dcd15e83e10737fc9801181b774d611f6
박현우 <ghwhsbsb123@gmail.com>
2019-08-17 20:09:26 +0900

    Change tab

--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,14 @@ app.set("port", process.env.PORT || 3000);
 app.use(logger("dev"));
 
 app.listen(app.get("port"), () => {
-	console.log(
-		"  App is running at http://localhost:%d in %s mode",
-		app.get("port"),
-		app.get("env")
-	);
-	console.log("  Press CTRL-C to stop\n");
+    console.log(
+        "  App is running at http://localhost:%d in %s mode",
+        app.get("port"),
+        app.get("env")
+    );
+    console.log("  Press CTRL-C to stop\n");
 });
 
 app.get("/", (req: Request, res: Response) => {
-	res.send("Hello, world!");
+    res.send("Hello, world!");
 });
