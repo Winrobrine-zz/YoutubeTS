a774c913b3d35bedcd497f304484fe4028f4dd6a
박현우 <ghwhsbsb123@gmail.com>
2019-08-20 01:23:39 +0900

    Scss: footer, form, and social login

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger("dev"));
 app.use((req, res, next) => {
-    res.locals.user = true;
+    res.locals.user = false;
     res.locals.routes = routes;
     next();
 });
