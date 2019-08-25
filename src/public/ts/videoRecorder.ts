026c74db4763ea798e985f2399666dd5e08fa4e1
박현우 <ghwhsbsb123@gmail.com>
2019-08-26 01:13:39 +0900

    Finish video recording

--- src/public/ts/videoRecorder.ts
+++ src/public/ts/videoRecorder.ts
@@ -8,6 +8,33 @@ const videoPreview = document.getElementById(
     "jsVideoPreview"
 ) as HTMLVideoElement;
 
+let videoRecorder: MediaRecorder;
+
+function onDataAvailable(event: BlobEvent) {
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(event.data);
+    link.download = "recorded.webm";
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+}
+
+function stopRecording() {
+    videoRecorder.stop();
+
+    videoPreview.pause();
+    (<MediaStream>videoPreview.srcObject)
+        .getTracks()
+        .forEach(track => track.stop());
+    videoPreview.srcObject = null;
+
+    recordButton.onclick = startRecording;
+    recordButton.innerHTML = "Start Recording";
+}
+
 async function startRecording() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({
@@ -17,17 +44,25 @@ async function startRecording() {
                 height: 720
             }
         });
+
         videoPreview.srcObject = stream;
         videoPreview.muted = true;
         videoPreview.play();
+
+        videoRecorder = new MediaRecorder(stream);
+        videoRecorder.ondataavailable = onDataAvailable;
+        videoRecorder.start();
+
+        recordButton.innerHTML = "Stop Recording";
+        recordButton.onclick = stopRecording;
     } catch (err) {
-        recordButton.innerHTML = "Cannot Record";
-        recordButton.removeEventListener("click", startRecording);
+        recordButton.innerHTML = `Cannot Record: ${err}`;
+        recordButton.onclick = null;
     }
 }
 
 function init() {
-    recordButton.addEventListener("click", startRecording);
+    recordButton.onclick = startRecording;
 }
 
 if (recorderContainer) {
